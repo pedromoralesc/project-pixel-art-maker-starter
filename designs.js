92da6d27aee0e8413b0d35645f577f83eef7552b
Pedro Morales <pedromoralesc@gmail.com>
2018-02-15 18:58:54 -0500

    initial commit

--- designs.js
+++ designs.js
@@ -2,9 +2,27 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+const canvas = document.getElementById("pixelCanvas")
+const size = document.getElementById("sizePicker");
+const heigh = document.getElementById("inputHeight");
+const weight = document.getElementById("inputWeight");
 
-function makeGrid() {
 
-// Your code goes here!
 
+function makeGrid(inputHeight,inputWeight) {
+    for(let rows = 0; rows < inputHeight; rows ++){
+        const trs = document.createElement("TR");
+        canvas.appendChild(trs);
+        
+        for( let columns = 0 ; columns <  inputWeight; columns ++){
+            const tds = document.createElement("TD");
+            trs.appendChild(tds);
+            
+        }
+    }
 }
+size.addEventListener("submit", function(e){
+    e.preventDefault();
+    makeGrid(heigh.value , weight.value);
+}, false);
+
